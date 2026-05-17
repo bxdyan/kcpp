3a9e48bba7ef547f98166b66938411b38809aadf
Bohdan Volskyi <volskyibohdan@gmail.com>
2026-05-17 18:45:47 +0200

    ZadArytmetykaWskaznikowZnajdowanie.cc - zadanie 3.3

---
+++ kcppZadania/ZadArytmetykaWskaznikowZnajdowanie.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int* maxElement(int* tab, int rozmiar) {
+    int* maks = tab;
+    for (int i = 1; i < rozmiar; i++) {
+        if (*(tab + i) > *maks) {
+            maks = tab + i;
+        }
+    }
+    return maks;
+}
+
+int main() {
+    int tab[] = {3, 7, 1, 9, 4, 6};
+    int rozmiar = 6;
+
+    cout << "tablica: ";
+    for (int i = 0; i < rozmiar; i++) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+
+    int* maks = maxElement(tab, rozmiar);
+    cout << "najwiekszy element: " << *maks << endl;
+
+    return 0;
+}
