980d628d11de71100956e2bf899d90d1f8c2e5f1
Bohdan Volskyi <volskyibohdan@gmail.com>
2026-05-17 18:43:18 +0200

    ZadArytmetykaWskaznikowSuma.cc - zadanie 3.2

---
+++ kcppZadania/ZadArytmetykaWskaznikowSuma.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int suma(int* tab, int rozmiar) {
+    int wynik = 0;
+    for (int i = 0; i < rozmiar; i++) {
+        wynik += *(tab + i);
+    }
+    return wynik;
+}
+
+int main() {
+    int tab[] = {1, 2, 3, 4, 5};
+    int rozmiar = 5;
+
+    cout << "tablica: ";
+    for (int i = 0; i < rozmiar; i++) {
+        cout << tab[i] << " ";
+    }
+    cout << endl;
+
+    cout << "suma: " << suma(tab, rozmiar) << endl;
+
+    return 0;
+}
