e3499bebb46605bf0a49bf74a232ed5c2671b601
Bohdan Volskyi <volskyibohdan@gmail.com>
2026-05-28 15:41:09 +0200

    ZadCinCoutFile.cc - zadanie 4.2

---
+++ kcppZadania/ZadCinCoutFile.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // zapis i odczyt z pliku pominiete bo linie w tresci zadania byly zakomentowane
+    cout << "to jest zwykly komunikat wypisany na ekran" << endl;
+
+    cerr << "to jest komunikat bledu zapisany do strumienia bledow" << endl;
+
+    return 0;
+}
