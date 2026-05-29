918cf45fb24364b5cee4df93ccae30f8ec9e5a57
Bohdan Volskyi <volskyibohdan@gmail.com>
2026-05-29 19:01:37 +0200

    LKsztalt.cc - zadanie 4.13

--- kcppBasic/src/ZadStudent/LKsztalt.cc
+++ kcppBasic/src/ZadStudent/LKsztalt.cc
@@ -2,25 +2,47 @@
 using namespace std;
 
 int main() {
-  int rozmiar;
-  cout << "Podaj rozmiar kwadratu: ";
-  cin >> rozmiar;
+    int rozmiar;
+    cout << "podaj rozmiar: ";
+    cin >> rozmiar;
 
-  cout << "\nKwadrat:" << endl;
-  for (int i = 0; i < rozmiar; i++) {
-    for (int j = 0; j < rozmiar; j++) {
-      cout << "* ";
+    cout << "\nkwadrat:" << endl;
+    for (int i = 0; i < rozmiar; i++) {
+        for (int j = 0; j < rozmiar; j++) {
+            cout << "* ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 
-  cout << "\nTrojkat prostokatny:" << endl;
-  for (int i = 0; i < rozmiar; i++) {
-    for (int j = 0; j <= i; j++) {
-      cout << "* ";
+    cout << "\ntrojkat prostokatny:" << endl;
+    for (int i = 0; i < rozmiar; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
 
-  return 0;
+    cout << "\ntrojkat rownoramienny:" << endl;
+    for (int i = 1; i <= rozmiar; i++) {
+        for (int j = i; j < rozmiar; j++) {
+            cout << "  ";
+        }
+        for (int j = 0; j < 2 * i - 1; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    cout << "\npiramida z liczb:" << endl;
+    for (int i = 1; i <= rozmiar; i++) {
+        for (int j = i; j < rozmiar; j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
