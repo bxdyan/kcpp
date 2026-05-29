c97767f7cf891fcc4d6a685ea5d370283abca62e
Bohdan Volskyi <volskyibohdan@gmail.com>
2026-05-29 19:01:55 +0200

    Zadanie 4.14

--- kcppBasic/src/ZadStudent/LWhileA.cc
+++ kcppBasic/src/ZadStudent/LWhileA.cc
@@ -2,17 +2,23 @@
 using namespace std;
 
 int main() {
-  int liczba, suma = 0;
+    int liczba, suma = 0, licznik = 0;
 
-  cout << "Wprowadzaj liczby (0 konczy program):" << endl;
-  cin >> liczba;
-
-  while (liczba != 0) {
-    suma += liczba;
+    cout << "wprowadzaj liczby (0 konczy program):" << endl;
     cin >> liczba;
-  }
 
-  cout << "Suma wprowadzonych liczb: " << suma << endl;
+    while (liczba != 0) {
+        if (liczba <= 0) {
+            cout << "liczba musi byc dodatnia" << endl;
+        } else {
+            suma += liczba;
+            licznik++;
+        }
+        cin >> liczba;
+    }
 
-  return 0;
+    cout << "suma wprowadzonych liczb: " << suma << endl;
+    cout << "liczba wprowadzonych wartosci: " << licznik << endl;
+
+    return 0;
 }
